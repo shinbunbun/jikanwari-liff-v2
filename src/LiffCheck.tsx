192c69e37fd04435c2b1fe36b122d0c02491548d
shinbunbun <takamuneyuto@gmail.com>
2022-07-29 15:34:50 +0900

    fix liffCheck

--- src/LiffCheck.tsx
+++ src/LiffCheck.tsx
@@ -1,15 +1,18 @@
-import liff from '@line/liff/dist/lib';
-import { useLiffInit } from './hooks/liff'
+import { useLiffInit, useLiffProfile } from './hooks/liff'
+import  liff from "@line/liff";
+import { LiffMockPlugin } from '@line/liff-mock';
 
 function LiffCheck() {
-  const liffObject = useLiffInit({ liffId: "dummy", mock: true });
-  console.log(liffObject);
+  const liff = useLiffInit({ liffId: "aaa", mock: true });
+  console.log(liff);
+  console.log(liff?.isLoggedIn());
+  console.log(liff?.id);
+  console.log(liff?.getOS());
+  console.log(liff?.getVersion());
+  console.log(liff?.getContext());
+  console.log(liff?.getAccessToken());
+  console.log(liff?.getProfile());
 
-  if (liffObject) {
-    if (!liff.isInClient()) liff.login();
-    const profile = liff.getProfile();
-    console.log(profile);
-  }
 
   return (
     <h1>Hello, LIFF</h1>
