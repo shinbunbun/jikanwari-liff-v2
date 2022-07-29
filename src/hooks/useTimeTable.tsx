af86c2e6af861887e581592c466bb0d67439c084
shinbunbun <takamuneyuto@gmail.com>
2022-07-29 18:10:35 +0900

    fix uuid

--- src/hooks/useTimeTable.tsx
+++ src/hooks/useTimeTable.tsx
@@ -28,7 +28,7 @@ const getTimeTable = (userId: string) => {
     sat: "【土曜日】\n1.コミュ英3\n2.化学\n3.化学\n4.英演",
     thu: "【木曜日】\n1.化学\n2.理演\n3.数3\n4.数3\n5.英表2\n6.体育",
     tue: "【火曜日】\n1.数3\n2.数3\n3.現文\n4.コミュ英3\n5.国英選\n6.社数演",
-    uuid: "174cd46ede6102chftd52",
+    uuid: "XXXXX",
     wed: "【水曜日】\n1.英表2\n2.体育\n3.理選\n4.数3\n5.数3\n6.LHR"
   } as TimeTable;
   return dummyTimeTable;
