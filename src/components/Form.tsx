b4076d9dad52300935947eff365b2f12ce169533
shinbunbun <takamuneyuto@gmail.com>
2022-08-03 15:50:41 +0900

    fmt

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { FC } from 'react'
-import { Input as ChakraInput, FormLabel as ChakraFormLabel, HStack } from '@chakra-ui/react'
-import { FieldValues, useForm, UseFormRegister } from "react-hook-form";
+import {
+  Input as ChakraInput,
+  FormLabel as ChakraFormLabel,
+  HStack,
+} from '@chakra-ui/react'
+import { FieldValues, useForm, UseFormRegister } from 'react-hook-form'
 
 type InputFormProps = {
   label: string
@@ -9,13 +13,9 @@ type InputFormProps = {
 }
 
 export const InputForm: FC<InputFormProps> = ({ label, register }) => (
-  <HStack
-    paddingBottom="12px">
+  <HStack paddingBottom="12px">
     <ChakraFormLabel>{label}</ChakraFormLabel>
-    <ChakraInput
-      {...register(label)}
-      maxWidth="50%"
-    />
+    <ChakraInput {...register(label)} maxWidth="50%" />
   </HStack>
 )
 
@@ -24,21 +24,20 @@ type FormProps = {
 }
 
 export const Form: FC<FormProps> = ({ labels }) => {
-  const {
-    register,
-    handleSubmit,
-  } = useForm();
+  const { register, handleSubmit } = useForm()
 
   const onSubmit = (data: unknown) => {
     // eslint-disable-next-line no-alert
-    alert(JSON.stringify(data));
-  };
+    alert(JSON.stringify(data))
+  }
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     <form onSubmit={handleSubmit(onSubmit)}>
-      {labels.map((label) => <InputForm label={label} register={register} />)}
+      {labels.map((label) => (
+        <InputForm label={label} register={register} />
+      ))}
       {/* <ChakraInput type="submit" value="確定" /> */}
     </form>
-  );
+  )
 }
